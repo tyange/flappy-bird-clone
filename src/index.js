1e7a187ef9e7d99f74132ce2000097c491f0b7ab
tyange <usun16@gmail.com>
2024-01-22 08:17:26 +0900

    grouping pipes & dynamic pipe horizontal distance

--- src/index.js
+++ src/index.js
@@ -22,11 +22,10 @@ const config = {
 const PIPES_TO_RENDER = 4;
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
-let pipeHorizontalDistance = 0;
+let pipes = null;
 
 const pipeVerticalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [500, 550];
 
 const flapVelocity = 300;
 const initialBirdPosition = {
@@ -48,25 +47,16 @@ function create() {
     .setOrigin(0);
   bird.body.gravity.y = 400;
 
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    pipeHorizontalDistance += 400;
-    let pipeVerticalDistance = Phaser.Math.Between(
-      ...pipeVerticalDistanceRange
-    );
-    let pipeVerticalPosition = Phaser.Math.Between(
-      0 + 20,
-      config.height - 20 - pipeVerticalDistance
-    );
-    upperPipe = this.physics.add
-      .sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe")
-      .setOrigin(0, 1);
-    lowerPipe = this.physics.add
-      .sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, "pipe")
-      .setOrigin(0, 0);
+  pipes = this.physics.add.group();
 
-    upperPipe.body.velocity.x = -200;
-    lowerPipe.body.velocity.x = -200;
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-200);
 
   this.input.on("pointerdown", function () {
     console.log("pressing mouse button");
@@ -79,6 +69,38 @@ function update(time, delta) {
   if (bird.y > config.height || bird.y < 0 - bird.height) {
     restartBirdPosition();
   }
+}
+
+function placePipe(uPipe, lPipe) {
+  const rightMostX = getRightMostPipe();
+  const pipeVerticalDistance = Phaser.Math.Between(
+    ...pipeVerticalDistanceRange
+  );
+  const pipeVerticalPosition = Phaser.Math.Between(
+    0 + 20,
+    config.height - 20 - pipeVerticalDistance
+  );
+  const pipeHorizontalDistance = Phaser.Math.Between(
+    ...pipeHorizontalDistanceRange
+  );
+
+  uPipe.x = rightMostX + pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+// pipes 중에서 x 값이 가장 큰 pipe의 x 값을 추출
+// 위의 placePipe 함수에서 rightMostX, 즉 x의 최대값, 제일 마지막에 배치된 pipe의 x 값을 가리킴
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function (pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+
+  return rightMostX;
 }
 
 function restartBirdPosition() {
