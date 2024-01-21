08ea5a1fa460d82da8d4eb8c28ed937fd4222d01
tyange <usun16@gmail.com>
2024-01-21 17:59:54 +0900

    add flap action

--- src/index.js
+++ src/index.js
@@ -8,41 +8,47 @@ const config = {
   physics: {
     // Arcade physics plugin, manages physics simulation
     default: "arcade",
+    arcade: {
+      gravity: {
+        y: 400,
+      },
+      debug: true,
+    },
   },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
-// Loading assets, such as images, music, animations ....
 function preload() {
-  // this context = scene
-  // contains functions and properties we can use
   this.load.image("sky", "assets/sky.png");
   this.load.image("bird", "assets/bird.png");
 }
 
 let bird = null;
+let flapVelocity = 300;
+let totalDelta = null;
 
 function create() {
-  // x - 400
-  // y - 300
-  // key of the image
   this.add.image(0, 0, "sky").setOrigin(0);
-  // middle of the height, 1/10 width
+
   bird = this.physics.add
     .sprite(config.width * 0.1, config.height / 2, "bird")
     .setOrigin(0);
 
-  // gravity: 시간이 지날 수록 속도가 증가
-  // velocity: 속도 고정
-  bird.body.velocity.y = 200;
+  this.input.on("pointerdown", function () {
+    console.log("pressing mouse button");
+  });
+
+  this.input.keyboard.on("keydown-SPACE", flap);
 }
 
-// 60fps
-// 60 times per second
-// 60 * 16ms = 1000ms
 function update(time, delta) {}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
+}
 
 new Phaser.Game(config);
