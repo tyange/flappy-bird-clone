5b8f15fb8e736a4a525e5108416c47a023a1bb67
tyange <usun16@gmail.com>
2024-01-21 17:14:44 +0900

    gravity & velocity

--- src/index.js
+++ src/index.js
@@ -31,9 +31,18 @@ function create() {
   // key of the image
   this.add.image(0, 0, "sky").setOrigin(0);
   // middle of the height, 1/10 width
-  bird = this.add
+  bird = this.physics.add
     .sprite(config.width * 0.1, config.height / 2, "bird")
     .setOrigin(0);
+
+  // gravity: 시간이 지날 수록 속도가 증가
+  // velocity: 속도 고정
+  bird.body.velocity.y = 200;
 }
+
+// 60fps
+// 60 times per second
+// 60 * 16ms = 1000ms
+function update(time, delta) {}
 
 new Phaser.Game(config);
