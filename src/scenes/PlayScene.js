5cd7a70677c3720cb67b1d869ea35eebb12cb311
tyange <usun16@gmail.com>
2024-01-28 20:41:23 +0900

    add bird sprite

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -99,6 +99,8 @@ class PlayScene extends BaseScene {
   createBird() {
     this.bird = this.physics.add
       .sprite(this.config.startPosition.x, this.config.startPosition.y, "bird")
+      .setFlipX(true)
+      .setScale(3)
       .setOrigin(0);
     this.bird.body.gravity.y = 600;
     this.bird.setCollideWorldBounds();
